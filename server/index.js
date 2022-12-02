a71c9d2c7ab7089126be815ea37c4ea5ceb48114
khampanpare88 <khampanpare88@gmail.com>
2022-12-02 14:54:59 +0700

    add route

--- server/index.js
+++ server/index.js
@@ -3,13 +3,17 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-import coursesRouters from './routes/courses.js';
-import skillRouters from './routes/skills.js';
+import getCourses from './routes/courses.js';
+import getSkills from './routes/skills.js';
 import updateCoursesSkill from './routes/courses.js';
+// add new skill
 import newSkill from './routes/skills.js';
 import getCareer from './routes/career.js';
+// add new career
 import newCareer from './routes/career.js';
 import getStudent from './routes/student.js';
+// add course for student
+import addCourses from './routes/student.js';
 
 const app = express();
 
@@ -17,10 +21,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use('/courses' ,coursesRouters ,updateCoursesSkill);
-app.use('/skills',skillRouters ,newSkill);
+app.use('/courses' ,getCourses ,updateCoursesSkill);
+app.use('/skills',getSkills ,newSkill);
 app.use('/career' , getCareer ,newCareer);
-app.use('/student' , getStudent);
+app.use('/student' , getStudent, addCourses);
 
 
 //const CONNECTION_URL = 'mongodb+srv://project:491@skillmapping.s8p064c.mongodb.net/?retryWrites=true&w=majority'
