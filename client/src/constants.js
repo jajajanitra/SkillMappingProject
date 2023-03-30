4cec07a4ff2072eb57c955187f0562330f17ef72
janitra <jajajanitra@gmail.com>
2023-03-30 12:08:37 +0700

    env template

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,11 +1,4 @@
+export const Server_URL = "http://localhost:5001";
 
-export const Server_URL = "https://server-skillmapping-production.up.railway.app";
-
-export const Course_URL = "https://server-skillmapping-production.up.railway.app/courses";
-export const Skill_URL = "https://server-skillmapping-production.up.railway.app/skills";
-
-
-// export const Server_URL = "http://localhost:5001";
-
-// export const Course_URL = "http://localhost:5001/courses";
-// export const Skill_URL = "http://localhost:5001/skills";
+export const Course_URL = "http://localhost:5001/courses";
+export const Skill_URL = "http://localhost:5001/skills";
